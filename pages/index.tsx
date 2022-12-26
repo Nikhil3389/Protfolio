2c31596928667f4c2679a5f4391cb5093a0515fe
Nikhil3389 <71307854+Nikhil3389@users.noreply.github.com>
2022-12-26 21:41:09 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,10 @@ import { GetServerSidePropsContext,GetStaticPropsContext,
 
       <motion.div 
       variants={ routeAnimation}
-      initial= "initial " 
+      initial= "initial" 
        animate = "animate"
         className='flex flex-col flex-grow px-6 pt-1'>
-        <h5 className='my-3 font-medium'>I am currently pursuing Bachelor of Technology  Degree(Third Year) in Computer Science Engineering from Lovely Professional University, Punjab. I am good at mathematics and problem-solving. Languages are known by me: Java, C, C++ and Python . | Competitive Programmer | | Ethical Hacker | | Full Stack Developer | | div 3 Codeshef 150+| | Leetcode 50+| | GFG 200+ | | Level 5 @ Coding Ninja | | 5⭐C++, Python @Hacker Rank | | Member @ShappireLPU </h5>
+        <h5 className='my-3 font-medium'>I am currently pursuing Bachelor of Technology  Degree(Second Year) in Computer Science Engineering from Lovely Professional University, Punjab. I am good at mathematics and problem-solving. Languages are known by me: Java, C, C++. |Competitive Programmer | | Ethical Hacker | | Full Stack Developer | | div 3 Codeshef 150+| | Leetcode 50+| | GFG 200+ | | Level 5 @ Coding Ninja | | 5⭐C++, Python @Hacker Rank | | Member @ShappireLPU </h5>
      
       <div className='flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100'
       style={{marginLeft:'-1.5rem', marginRight: '-1.5rem' }}>
